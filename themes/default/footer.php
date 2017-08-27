9bf4a0efe6806317d14869aae0ffa9dbf94b6767
edomasig <edomasig@gmail.com>
2017-08-28 01:22:03 +0800

    change footer

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -2,7 +2,7 @@
     <hr />
     <footer class="footer">
         <div class="container">
-            <p>Powered by <a href="http://cibonfire.com" target="_blank">Bonfire <?php echo BONFIRE_VERSION; ?></a></p>
+            <p>Powered by <a href="http://http://www.verticaldesignevolution.com/" target="_blank">Vertical Design </a></p>
         </div>
     </footer>
     <?php endif; ?>
